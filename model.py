d02402ec6df5a2d5005b4b4a60ca6a4a9d24d75f
Sergey Kishchenko <voidwrk@gmail.com>
2016-09-15 10:37:31 -0800

    Created model as a separate file

---
+++ model.py
@@ -0,0 +1,88 @@
+import tensorflow as tf
+from tensorflow.python.ops import ctc_ops as ctc
+from tensorflow.python.ops import rnn_cell
+from tensorflow.python.ops.rnn import bidirectional_rnn
+import numpy as np
+import prettytensor as pt
+
+class CtcModel(object):
+    def __init__(self, max_time_steps, num_features, num_classes):
+        self._create_graph(num_hidden=128,
+                           batch_size=4,
+                           max_time_steps=max_time_steps,
+                           num_features=num_features,
+                           conv_depth=10,
+                           num_classes=num_classes)
+
+    def _create_graph(self, num_hidden, batch_size, max_time_steps, num_features, conv_depth, num_classes):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            # e.g: log filter bank or MFCC features
+            # Has size [batch_size, max_time_steps, num_features], but the
+            # batch_size and max_stepsize can vary along each step
+            self.inputs = tf.placeholder(tf.float32, [batch_size, max_time_steps, num_features])
+
+            # Here we use sparse_placeholder that will generate a
+            # SparseTensor required by ctc_loss op.
+            self.targets = tf.sparse_placeholder(tf.int32)
+
+            # 1d array of size [batch_size]
+            self.seq_len = tf.placeholder(tf.int32, [batch_size])
+
+            # Defining the cell
+            # Can be:
+            #   tf.nn.rnn_cell.RNNCell
+            #   tf.nn.rnn_cell.GRUCell
+            cell = tf.nn.rnn_cell.LSTMCell(num_hidden, state_is_tuple=True)
+
+            inputsW = (pt.wrap(tf.expand_dims(self.inputs, -1))
+                         .conv2d(3, conv_depth, activation_fn=tf.nn.relu)
+                         .conv2d(3, conv_depth, activation_fn=tf.nn.relu))
+            # The second output is the last state and we will no use that
+            inputsW = tf.reshape(inputsW, [batch_size, max_time_steps, num_features * conv_depth])
+            outputs, _ = tf.nn.dynamic_rnn(cell, inputsW, self.seq_len, dtype=tf.float32)
+            # outputs, _ = tf.nn.dynamic_rnn(cell, inputs, self.seq_len, dtype=tf.float32)
+
+            shape = tf.shape(self.inputs)
+            batch_s, max_timesteps = shape[0], shape[1]
+
+            # Reshaping to apply the same weights over the timesteps
+            outputs = tf.reshape(outputs, [-1, num_hidden])
+
+            # Truncated normal with mean 0 and stdev=0.1
+            # Tip: Try another initialization
+            # see https://www.tensorflow.org/versions/r0.9/api_docs/python/contrib.layers.html#initializers
+            W = tf.Variable(tf.truncated_normal([num_hidden,
+                                                 num_classes],
+                                                stddev=0.1))
+            # Zero initialization
+            # Tip: Is tf.zeros_initializer the same?
+            b = tf.Variable(tf.constant(0., shape=[num_classes]))
+
+            # Doing the affine projection
+            logits = tf.matmul(outputs, W) + b
+
+            # Reshaping back to the original shape
+            logits = tf.reshape(logits, [batch_s, -1, num_classes])
+
+            # Time major
+            logits = tf.transpose(logits, (1, 0, 2))
+
+            self.loss = tf.reduce_mean(ctc.ctc_loss(logits, self.targets, self.seq_len))
+            self.logitsMaxTest = tf.slice(tf.argmax(logits, 2), [0, 0], [self.seq_len[0], 1])
+
+            self.optimizer = tf.train.AdamOptimizer().minimize(self.loss)
+
+            self.predictions = tf.to_int32(ctc.ctc_beam_search_decoder(logits, self.seq_len)[0][0])
+
+            self.errorRate = tf.reduce_sum(tf.edit_distance(self.predictions, self.targets, normalize=False)) / \
+                        tf.to_float(tf.size(self.targets.values))
+
+    def run_predictions(self, session, inputs, seq_len):
+        return session.run(self.predictions, feed_dict={self.inputs: inputs, self.seq_len: seq_len})
+
+    def run_train_step(self, session, inputs, seq_len, targets):
+        fd = {self.inputs: inputs, self.targets: targets, self.seq_len: seq_len}
+        return session.run([self.optimizer, self.loss, self.errorRate, self.logitsMaxTest, self.predictions], feed_dict=fd)
+
+
--- train_ctc.py
+++ train_ctc.py
@@ -9,10 +9,10 @@ from tensorflow.python.ops import rnn_cell
 from tensorflow.python.ops.rnn import bidirectional_rnn
 import numpy as np
 import prettytensor as pt
-# import cv2
 import json
-# from utils import load_batched_data
+import sys
 
+from model import CtcModel
 with open("model_input.json", "r") as fp:
     model_input = json.load(fp)
 
@@ -129,83 +129,11 @@ print('Loading data')
 batchedData, maxTimeSteps, totalN = load_batched_data(model_input, batchSize)
 
 ####Define graph
-print('Defining graph')
-graph = tf.Graph()
-with graph.as_default():
-
-    # e.g: log filter bank or MFCC features
-    # Has size [batch_size, max_stepsize, num_features], but the
-    # batch_size and max_stepsize can vary along each step
-    inputs = tf.placeholder(tf.float32, [batchSize, maxTimeSteps, nFeatures])
-
-    # Here we use sparse_placeholder that will generate a
-    # SparseTensor required by ctc_loss op.
-    targets = tf.sparse_placeholder(tf.int32)
-
-    # 1d array of size [batch_size]
-    seq_len = tf.placeholder(tf.int32, [batchSize])
-
-    # Defining the cell
-    # Can be:
-    #   tf.nn.rnn_cell.RNNCell
-    #   tf.nn.rnn_cell.GRUCell
-    cell = tf.nn.rnn_cell.LSTMCell(nHidden, state_is_tuple=True)
-
-    # Stacking rnn cells
-    # num_layers = 1
-    # stack = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers,
-    #                                     state_is_tuple=True)
-
-    inputsW = (pt.wrap(tf.expand_dims(inputs, -1))
-                 .conv2d(3, 10, activation_fn=tf.nn.relu)
-                 .conv2d(3, 10, activation_fn=tf.nn.relu))
-    # The second output is the last state and we will no use that
-    inputsW = tf.reshape(inputsW, [batchSize, maxTimeSteps, nFeatures * 10])
-    outputs, _ = tf.nn.dynamic_rnn(cell, inputsW, seq_len, dtype=tf.float32)
-    # outputs, _ = tf.nn.dynamic_rnn(cell, inputs, seq_len, dtype=tf.float32)
-
-    shape = tf.shape(inputs)
-    batch_s, max_timesteps = shape[0], shape[1]
-
-    # Reshaping to apply the same weights over the timesteps
-    outputs = tf.reshape(outputs, [-1, nHidden])
-
-    # Truncated normal with mean 0 and stdev=0.1
-    # Tip: Try another initialization
-    # see https://www.tensorflow.org/versions/r0.9/api_docs/python/contrib.layers.html#initializers
-    W = tf.Variable(tf.truncated_normal([nHidden,
-                                         nClasses],
-                                        stddev=0.1))
-    # Zero initialization
-    # Tip: Is tf.zeros_initializer the same?
-    b = tf.Variable(tf.constant(0., shape=[nClasses]))
-
-    # Doing the affine projection
-    logits = tf.matmul(outputs, W) + b
-
-    # Reshaping back to the original shape
-    logits = tf.reshape(logits, [batch_s, -1, nClasses])
-
-    # Time major
-    logits = tf.transpose(logits, (1, 0, 2))
-
-    loss = tf.reduce_mean(ctc.ctc_loss(logits, targets, seq_len))
-    logitsMaxTest = tf.slice(tf.argmax(logits, 2), [0, 0], [seq_len[0], 1])
-
-    optimizer = tf.train.AdamOptimizer().minimize(loss)
-
-    # Option 2: tf.contrib.ctc.ctc_beam_search_decoder
-    # (it's slower but you'll get better results)
-    predictions = tf.to_int32(ctc.ctc_beam_search_decoder(logits, seq_len)[0][0])
-
-    # Inaccuracy: label error rate
-    errorRate = tf.reduce_sum(tf.edit_distance(predictions, targets, normalize=False)) / \
-                tf.to_float(tf.size(targets.values))
-    # ler = tf.reduce_mean(tf.edit_distance(tf.cast(predictions[0], tf.int32),
-    #                                       targets))
+print('Defining model')
+ctc_model = CtcModel(maxTimeSteps, nFeatures, nClasses)
 
 ####Run session
-with tf.Session(graph=graph) as session:
+with tf.Session(graph=ctc_model.graph) as session:
     print('Initializing')
     tf.initialize_all_variables().run()
     saver = tf.train.Saver()
@@ -220,7 +148,7 @@ with tf.Session(graph=graph) as session:
                 "second_bin": "/Users/tilarids/dev/decl/extract_img_data/0278d69820395cf130f098f79b46caa62023627a9a7362295e2c5489.pdf.1.png.second.bin.npy"
             }] * batchSize, batchSize)
         batchInputs, batchTargetSparse, batchSeqLengths = batchedDataPred[0]
-        pred = session.run(predictions, feed_dict={inputs: batchInputs, seq_len: batchSeqLengths})
+        pred = ctc_model.run_predictions(session, batchInputs, batchSeqLengths)
         predDense = session.run(tf.sparse_to_dense(pred.indices, pred.shape, pred.values))
         # print("Prediction:", pred.values)
         print("Prediction:", predDense)
@@ -232,11 +160,8 @@ with tf.Session(graph=graph) as session:
         batchRandIxs = np.random.permutation(len(batchedData)) #randomize batch order
         for batch, batchOrigI in enumerate(batchRandIxs):
             batchInputs, batchTargetSparse, batchSeqLengths = batchedData[batchOrigI]
-            # batchTargetIxs, batchTargetVals, batchTargetShape = batchTargetSparse
-            feedDict = {inputs: batchInputs, targets: batchTargetSparse, seq_len: batchSeqLengths}
-            _, l, er, lmt, pr = session.run([optimizer, loss, errorRate, logitsMaxTest, predictions], feed_dict=feedDict)
+            _, l, er, lmt, pr = ctc_model.run_train_step(session, batchInputs, batchSeqLengths, batchTargetSparse)
             print(np.unique(lmt)) #print unique argmax values of first sample in batch; should be blank for a while, then spit out target values
-            # print(pr, batchTargetSparse)
             if (batch % 1) == 0:
                 print('Minibatch', batch, '/', batchOrigI, 'loss:', l)
                 print('Minibatch', batch, '/', batchOrigI, 'error rate:', er)
